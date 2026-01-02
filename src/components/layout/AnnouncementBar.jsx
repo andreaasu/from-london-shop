a8a95224a1c22510f3a08ed01995a177cf77236b
andreaasu <andreaaziz83@gmail.com>
2026-01-02 14:23:06 +0200

    4k not 3k

--- src/components/layout/AnnouncementBar.jsx
+++ src/components/layout/AnnouncementBar.jsx
@@ -1,7 +1,7 @@
 export default function AnnouncementBar() {
     return (
         <div className="bg-black text-white text-xs font-bold text-center py-2 px-4 uppercase tracking-widest">
-            Free Standard Delivery on Orders Over 3000 LE | Cash on Delivery Available
+            Free Standard Delivery on Orders Over 4000 LE | Cash on Delivery Available
         </div>
     );
 }
