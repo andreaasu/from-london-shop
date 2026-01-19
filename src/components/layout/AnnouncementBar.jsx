946608ba139aeeef5bb77222a6ac1fec23b76147
andreaasu <andreaaziz83@gmail.com>
2026-01-19 21:14:26 +0200

    announcments

--- src/components/layout/AnnouncementBar.jsx
+++ src/components/layout/AnnouncementBar.jsx
@@ -1,7 +1,7 @@
 export default function AnnouncementBar() {
     return (
         <div className="bg-black text-white text-xs font-bold text-center py-2 px-4 uppercase tracking-widest">
-            Free Standard Delivery on Orders Over 4000 LE | Cash on Delivery Available
+            All Items Are Immediately Available
         </div>
     );
 }
