31010eda52a14e789bd22d309672a2296bd88830
Andrea Aziz <andreaaziz@gmail.com>
2026-03-10 01:48:02 +0200

    maggie

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -44,8 +44,7 @@ export default function Navbar() {
 
                     {/* Logo */}
                     <Link to="/" className="text-2xl font-black tracking-tight uppercase flex-shrink-0">
-                        From London
-                    </Link>
+                        MAGGIES STORE                    </Link>
 
                     {/* Desktop Nav */}
                     <div className="hidden md:flex space-x-8 font-medium">
