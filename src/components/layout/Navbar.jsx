08d38f3cf529da26727763d079a2eab5e3f9f85d
Andrea Aziz <andreaaziz@gmail.com>
2026-03-09 17:10:03 +0200

    test

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -44,7 +44,7 @@ export default function Navbar() {
 
                     {/* Logo */}
                     <Link to="/" className="text-2xl font-black tracking-tight uppercase flex-shrink-0">
-                        From London
+                        From London Rancy
                     </Link>
 
                     {/* Desktop Nav */}
